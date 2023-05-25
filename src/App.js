45d6be0eef2eea4dc4de8e5a1b7ada96234cea7c
elll0999 <bunaz7042@gmail.com>
2023-05-25 15:38:07 +0600

    db.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Footer from './components/footer/Footer'
-import Navbar from './components/footer/Navabar/Navbar'
+
+
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
+  
       <Footer/>
     </div>
   )
