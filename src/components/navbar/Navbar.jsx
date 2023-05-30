1fa8b3bf97433b1967b04e880cadfd2edb8efa2a
elll0999 <bunaz7042@gmail.com>
2023-05-30 15:23:09 +0600

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SearchIcon from "@mui/icons-material/Search";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
-import "../navbar/Navbar.css";
+import "../Navbar/Navbar.css";
 
 const Navbar = () => {
   return (
